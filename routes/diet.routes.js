72a3c58626e72ee7bbedb4a8a93ea5d3df49c44a
Dani Pardo <danielp.5@hotmail.com>
2022-07-01 10:05:54 +0200

    hard beggining

--- routes/diet.routes.js
+++ routes/diet.routes.js
@@ -5,23 +5,33 @@ const mongoose= require("mongoose")
 const User= require("../models/User.model")
 const Diet= require("../models/Diet.model");
 const isLoggedIn = require("../middleware/isLoggedIn");
+const isLoggedOut = require("../middleware/isLoggedOut");
 
-router.post("/", (req, res, next)=>{
+
+router.post("/", isLoggedIn, (req, res, next)=>{
     const {edad, genero, peso, altura, actividad} = req.body
-    console.log(req.body)
-    if(genero === "hombre"){
-      let GEB= (10*peso)+(6.25*altura)-(5*edad)+5
-      const GET= GEB*actividad
-      res.send(`Your diary kcal needs are ${GET}`)
-    }
-    else {
-      let GEB = (10*peso)+(6.25*altura)-(5*edad)-161
-      const GET= GEB*actividad
-      res.send(`Your diary kcal needs are ${GET}`)
-    }
-})  
+    
 
-router.get('/', isLoggedIn, (req,res, next)=>{
+    if(genero === "hombre"){
+        const GEB= (10*peso)+(6.25*altura)-(5*edad)+5
+        const GET= GEB+parseInt(actividad,10)
+        res.send(`Your diary caloric consume is ${GET} Kcal`)
+       // .then(()=>User.findByIdAndUpdate(req.session._id, {GET: GET}, {new:true}))
+       // .then((user)=>{res.redirect("/")})
+      }
+      else {
+        const GEB = (10*peso)+(6.25*altura)-(5*edad)-161
+        const GET= GEB+parseInt(actividad,10)
+        res.send(`Your diary caloric consume is ${GET} Kcal`)
+       // .then(()=>User.findByIdAndUpdate(req.session._id, {GET: GET}, {new:true}))
+       // .then((user)=>{res.redirect("/")})
+      }
+    
+    })
+
+
+
+router.get('/', (req,res, next)=>{
     res.render('diet')
 })
 
